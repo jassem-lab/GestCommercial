7f66639cf0e957f753b5d1a0fb2fe94b4f59d556
jassem-lab <jassemgaaloul123@gmail.com>
2022-06-08 14:51:42 +0100

    ➖ Suppression détail log

--- page_js/supprimerFamilleProduit.php
+++ page_js/supprimerFamilleProduit.php
@@ -3,12 +3,16 @@ session_start();
 include('../connexion/cn.php');
 	
 	$id   = $_GET["ID"] ;
-	
-	$sql = "delete from `delta_famille_produit` WHERE id=".$id;
+    $req="select * from delta_emplacements  WHERE id=".$id;
+    $query=mysql_query($req);
+    while($enreg=mysql_fetch_array($query)){
+        $code = $enreg['code'];
+    }
+	$sql = "delete from `delta_emplacements` WHERE id=".$id;
 	$requete = mysql_query($sql) ;
     $dateheure=date('Y-m-d H:i:s');
     $iduser=$_SESSION['delta_IDUSER'];
-    $sql1="INSERT INTO `delta_log`(`dateheure`, `idutilisateur`, `document`, `action`, `iddocument`) VALUES ('".$dateheure."','".$iduser."','3','3','".$id."')";
+    $sql1="INSERT INTO `delta_log`(`dateheure`, `idutilisateur`, `document`, `action`, `iddocument`,`code`) VALUES ('".$dateheure."','".$iduser."','3','3','".$id."','".$code."'";
     $req=mysql_query($sql1);	
     echo '<SCRIPT LANGUAGE="JavaScript">document.location.href="../tabs.php?suc=1" </SCRIPT>';
 	
